3a45d762137c511268327b08ea7e8e04dd11047f
svmihar <svmihar@gmail.com>
2020-03-01 18:54:27 -0800

    :fire: added new xgbregressor (tapi bodoh, preprocessing masih ngasal), otw tpot

---
+++ tpot_test_1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.impute import SimpleImputer
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+imputer = SimpleImputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+# Average CV score on the training set was: -6.068083376489832e+16
+exported_pipeline = DecisionTreeRegressor(max_depth=3, min_samples_leaf=4, min_samples_split=18)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
